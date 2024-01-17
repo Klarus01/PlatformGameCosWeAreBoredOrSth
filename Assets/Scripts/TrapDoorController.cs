0dc9ba0ed4c9825ba78e60d77286a41dc10f9dec
Damian4553 <44645512+Damian4553@users.noreply.github.com>
2024-01-17 23:38:21 +0100

    add one way platform, trapdoors and lever

---
+++ Assets/Scripts/TrapDoorController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TrapDoorController : MonoBehaviour
+{
+    private SpriteRenderer thisSpriteRenderer;
+    private BoxCollider2D thisBoxCollider2D;
+    [SerializeField] private bool open;
+    [SerializeField] private Sprite openSprite;
+    [SerializeField] private Sprite closedSprite;
+
+    
+    private void Start()
+    {
+        thisSpriteRenderer = GetComponent<SpriteRenderer>();
+        thisBoxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    public void SwitchState()
+    {
+        if (open == false)
+        {
+            thisSpriteRenderer.sprite = openSprite;
+            open = true;
+            thisBoxCollider2D.isTrigger = true;
+        }
+        else
+        {
+            thisSpriteRenderer.sprite = closedSprite;
+            open = false;
+            thisBoxCollider2D.isTrigger = false;
+        }
+    }
+
+}

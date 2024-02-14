113eab3fd9f85c0df0f735226ea9efad14cce804
Klarus <ciuk21@gmail.com>
2024-02-14 20:54:04 +0100

    added to camera delayed movement

--- Assets/Scripts/CameraClamp.cs
+++ Assets/Scripts/CameraClamp.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class CameraClamp : MonoBehaviour
 {
     [SerializeField] private Transform targetToFollow;
+    [SerializeField] private float followSpeed = 5f;
 
     private void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(targetToFollow.position.x, -66f, 385f), Mathf.Clamp(targetToFollow.position.y, 8f, 100f), transform.position.z);
+        Vector3 targetPosition = new Vector3(
+            Mathf.Clamp(targetToFollow.position.x, -66f, 385f),
+            Mathf.Clamp(targetToFollow.position.y, 8f, 100f),
+            transform.position.z
+        );
+
+        Vector3 newPosition = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+
+        transform.position = newPosition;
     }
 }
